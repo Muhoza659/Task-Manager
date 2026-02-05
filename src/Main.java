2f53681ba2e68e32db4c97cae2430f7710d12840
Muhoza659 <muhozarosine36@gmail.com>
2026-02-05 16:12:03 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+    public static void main(String[] args) {
+
+        TaskManager manager = new TaskManager();
+
+        manager.addTask("Do homework");
+        manager.addTask("Attend meeting");
+        manager.addTask("Prepare presentation");
+        manager.addTask("Submit assignment");
+
+        try {
+           manager.addTask("");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        manager.displayTasks();
+
+
+        manager.sortTasks();
+        manager.displayTasks();
+
+
+        manager.searchTask("Submit assignment");
+        manager.searchTask("Go to gym");
+
+
+        manager.removeTask("Do homework");
+        manager.removeTask("Sleep");
+
+        manager.displayTasks();
+
+
+        manager.findLongestTask();
+
+        manager.getTaskByIndex(1);
+        manager.getTaskByIndex(10);
+
+    }
 }
